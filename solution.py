7e15fd6074895541a38579ac0edf5efe2f77590d
Meschanin26 <123255812+Meschanin26@users.noreply.github.com>
2023-04-07 00:42:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,19 +1,19 @@
-import pandas as pd
 import numpy as np
-import scipy
 from scipy.stats import norm
 
 
-chat_id = 470764857 # Ваш chat ID, не меняйте название переменной
 
+def solution(p:float, x:np.array) -> tuple:
+    # Рассчитываем выборочное среднее и выборочное стандартное отклонение
+    sample_mean = np.mean(x)
+    sample_std = np.std(x, ddof=1)
 
-def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
-    loc = x.mean()
-    jj=np.sum((x-loc)**2)*(len(x)-1)
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    gg=scipy.stats.chi2.ppf(1-alpha,1)
-    hh=scipy.stats.chi2.ppf(alpha,1)
-    left_side=np.sqrt(jj/gg)/21
-    right_side=np.sqrt(jj/hh)/21
-    return left_side,right_side
+    # Рассчитываем значение статистики Z-оценки для уровня доверия
+    z_critical = norm.ppf(1 - (1 - p) / 2)
+
+    # Рассчитываем левую и правую границы доверительного интервала
+    margin_error = z_critical * sample_std / np.sqrt(len(x))
+    lower_bound = sample_mean - margin_error
+    upper_bound = sample_mean + margin_error
+
+    return lower_bound, upper_bound
